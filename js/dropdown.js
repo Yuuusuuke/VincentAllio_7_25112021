f9edcecd49fb59c4075febbb1803f90fcbc19d2f
Yuusuke <masngelox7@gmail.com>
2021-11-29 21:46:49 +0100

    handle closing dropdown when clicking around else

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,18 +1,36 @@
+/* Global variable */
+var INGRETOOGLE = false, APPATOOGLE = false, USTENTOOGLE = false;
 
 /* Event listeners which handle opening or closing a dropdown */
-var INGToogle = false, APPToogle = false, USTToogle = false;
-
-document.getElementsByClassName("filters__dropdown")[0].addEventListener("click", function flip(){
-    !INGToogle ? openDropdown(this) : closeDropdown(this);
-    INGToogle = !INGToogle;
+document.getElementsByClassName("filters__dropdown")[0].addEventListener("keyup", function (){
+    if(this.children[0].value.length === 0){
+        closeDropdown(this);
+        INGRETOOGLE = false;
+    }
+    else{
+        openDropdown(this);
+        INGRETOOGLE = true;
+    }
 });
-document.getElementsByClassName("filters__dropdown")[1].addEventListener("click", function flip(){
-    !APPToogle ? openDropdown(this) : closeDropdown(this);
-    APPToogle = !APPToogle;
+document.getElementsByClassName("filters__dropdown")[1].addEventListener("keyup", function (){
+    if(this.children[0].value.length === 0){
+        closeDropdown(this);
+        APPATOOGLE = false;
+    }
+    else{
+        openDropdown(this);
+        APPATOOGLE = true;
+    }
 });
-document.getElementsByClassName("filters__dropdown")[2].addEventListener("click", function flip(){
-    !USTToogle ? openDropdown(this) : closeDropdown(this);
-    USTToogle = !USTToogle;
+document.getElementsByClassName("filters__dropdown")[2].addEventListener("keyup", function (){
+    if(this.children[0].value.length === 0){
+        closeDropdown(this);
+        USTENTOOGLE = false;
+    }
+    else{
+        openDropdown(this);
+        USTENTOOGLE = true;
+    }
 });
 
 /**
@@ -36,4 +54,29 @@ function closeDropdown(element){
     element.classList.remove("expend");
     element.classList.remove("expend--2");
     element.children[2].classList.remove("show");
+}
+
+window.onclick = function(event){
+    if(INGRETOOGLE){
+        if(!event.target.matches(".ingredient")){
+            closeDropdown(document.getElementsByClassName("filters__dropdown")[0]);
+            INGRETOOGLE = false;
+        }
+    }
+
+    if(APPATOOGLE){
+        console.log(event.target.matches(".appareil"));
+        if(!event.target.matches(".appareil")){
+            closeDropdown(document.getElementsByClassName("filters__dropdown")[1]);
+            APPATOOGLE = false;
+        }
+    }
+
+    if(USTENTOOGLE){
+        console.log(event.target.matches(".ustensile"));
+        if(!event.target.matches(".ustensile")){
+            closeDropdown(document.getElementsByClassName("filters__dropdown")[2]);
+            USTENTOOGLE = false;
+        }
+    }
 }
